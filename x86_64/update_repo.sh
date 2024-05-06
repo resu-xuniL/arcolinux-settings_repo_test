51e9a3eaf60eb0e7add0854172ed6440780a774f
resu-xuniL <linux@lavache.com>
2024-05-06 17:13:00 +0200

    Release 22

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -2,14 +2,4 @@
 
 rm arcolinux-settings_repo_test*
 
-tput setaf 3
-echo "Repo add"
-echo
-tput sgr0
-
 repo-add -n -R arcolinux-settings_repo_test.db.tar.gz *.pkg.tar.zst
-
-tput setaf 3
-echo "Repo Updated"
-echo
-tput sgr0
