9370793ec5aaeea5650c471f943fe10b239a2660
resu-xuniL <linux@lavache.com>
2024-05-06 17:19:03 +0200

    Release 22 23

--- up.sh
+++ up.sh
@@ -17,28 +17,19 @@
 
 
 # Creating the databases
-cd x86_64
-sh update_repo.sh
-cd ..
-
-#workdir=$(pwd)
+rm arcolinux-settings_repo_test*
+repo-add -n -R arcolinux-settings_repo_test.db.tar.gz *.pkg.tar.zst
 
 # Below command will backup everything inside the project folder
 git add --all .
-
 
 # Set commit message
 input=$(ls x86_64 | grep '.pkg.tar.zst' | cut -d '-' -f6)
 
 tput setaf 3
-echo "####################################"
-echo " Commit message will be : Release ${input}"
-echo "####################################"
-echo
-tput sgr0
-
 # Committing to the local repository with a message containing the time details and commit text
 git commit -m "Release ${input}"
+tput sgr0
 
 # Push the local files to github
 git push -u origin main
